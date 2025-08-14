d75eaae3418cd6757986b3a5a48fe3fabd4bc5d4
Andre Lucho <andretlucho@gmail.com>
2025-08-14 12:05:10 -0300

    feat: Dashboard com fetch funcional

--- app-react-tmdb/src/components/Dashboard.jsx
+++ app-react-tmdb/src/components/Dashboard.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import Header from './Header';
 
 const Dashboard = ({ userInput }) => {
   const [data, setData] = useState(null);
+  const [movies, setMovies] = useState(null);
 
   const omdbKey = 'aae27b86';
 
@@ -23,16 +23,38 @@ const Dashboard = ({ userInput }) => {
     handleData();
   }, [userInput]);
 
-  // 1. utilizar o parâmetro &s --> para pagar o array de filmes com aquele nome
   // 2. com um botal e modal --> fazer novo fetch e utilizar o param &t para pegar detalhes do filme
 
   useEffect(() => {
     if (data) {
-      console.log(data);
+      const moviesData = data.Search.map((movie) => {
+        const { Title, Year, Poster } = movie;
+        const movies = {
+          Title,
+          Year,
+          Poster,
+        };
+        return movies;
+      });
+      setMovies(moviesData);
     }
   }, [data]);
 
-  return <div className="dashboard-container">{/* <p>{data}</p> */}</div>;
+  return (
+    <div className="dashboard-container">
+      <ul className="galery-container">
+        {movies &&
+          movies.map((movie, i) => (
+            <li key={i} className="img-container">
+              <img src={movie.Poster} alt="" />
+              <span>{movie.Title}</span>
+              <span>{movie.Year}</span>
+              <a>More Info</a>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Dashboard;
